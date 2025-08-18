d56060c6ae55957fc3c24e53fac2c04b3133913f
NurgaliyevS <nurgasab@gmail.com>
2025-08-18 23:14:15 +0300

    upd

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,7 +9,6 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
-
   return (
     <SessionProvider session={session}>
       <TooltipProvider>
@@ -18,6 +17,12 @@ export default function App({
         <Sonner />
         <Component {...pageProps} />
       </TooltipProvider>
+      <script
+        defer
+        data-website-id="68a3894d82f5236bced090dd"
+        data-domain="missnotes.com"
+        src="https://datafa.st/js/script.js"
+      ></script>
     </SessionProvider>
   );
 }
