c6ca9ee945c9375503eca706d940fae0e5a37e10
NurgaliyevS <nurgasab@gmail.com>
2025-08-18 22:37:01 +0300

    upd

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -2,6 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
     return (<Html lang="en">
       <Head>
+        <title>
+          MissNotes
+        </title>
         <meta name="description" content="Never leave a meeting without clear notes again - MissNotes"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="icon" href="/notebook-pen.png"/>
